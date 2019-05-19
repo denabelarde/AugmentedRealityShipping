4330b575a8072df4d120c49459cb5f25b064de8b
Denver Abelarde <denver@nexusgaming.tv>
2019-05-19 23:56:37 +0800

    Added object graph manifest class

--- app/src/main/java/com/augmentedreality/simplus/framework/SimplusComponent.java
+++ app/src/main/java/com/augmentedreality/simplus/framework/SimplusComponent.java
@@ -1,5 +1,10 @@
 package com.augmentedreality.simplus.framework;
 
+import com.augmentedreality.simplus.dashboard.DashboardModule;
+import com.augmentedreality.simplus.splash.SplashModule;
+import com.augmentedreality.simplus.util.UtilityModule;
+import com.augmentedreality.simplus.util.rx.RxUtilityModule;
+
 import javax.inject.Singleton;
 
 import dagger.BindsInstance;
@@ -9,7 +14,10 @@ import dagger.android.support.AndroidSupportInjectionModule;
 @Component(modules = {
     AndroidSupportInjectionModule.class,
     AppModule.class,
-
+    UtilityModule.class,
+    DashboardModule.class,
+    RxUtilityModule.class,
+    SplashModule.class
 })
 @Singleton
 public interface SimplusComponent {
